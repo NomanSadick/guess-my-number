26584ebbc153fff473103b49e588c85544a86015
NomanSadick <nomansadickjs@gmail.com>
2022-06-29 21:16:40 +0600

    some functionality done

---
+++ script.js
@@ -0,0 +1,25 @@
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct number'
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+let secretNumber = Math.round(Math.random() * 20);
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function(){
+   const guess =  document.querySelector('.guess').value;
+   console.log(guess, typeof guess);
+
+   if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+   
+   } 
+   
+   else if (guess == secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+} 
+})
